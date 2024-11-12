f2f0fee34b4c9c766fcf2900103e367a1441c8f5
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-13 02:54:25 +0600

    auth middleware remobe token data base

---
+++ src/middleware/aurh.middleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+
+    const token = req.cookies.accessToken || req?.header?.authorization?.split(" ")[1]
+    console.log(token );
+
+    if(!token){
+        return response.status(401).json({
+            message : "Provide token"
+        })
+    }
+    
+    const decode = jwt.verify(token, process.env.SECERET_KEY_ACCESS_TOKEN);
+    console.log(decode);
+    
+    if(!decode){
+        return response.status(401).json({
+            message : "unauthorized access",
+            error : true,
+            success : false
+        })
+    }
+
+    req.userId = decode._id
+    
+    next();
+
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
+
+export default auth;
