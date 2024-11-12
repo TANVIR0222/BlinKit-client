7f5d129193b19c978da5f79d66477b867c5308f8
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-13 02:09:23 +0600

    connect bd

---
+++ src/db/connectDB.js
@@ -0,0 +1,16 @@
+// const mongoose = require('mongoose');
+import mongoose from 'mongoose'
+
+export const connectDB = async () =>{
+    try {        
+        await mongoose.connect(process.env.DB)
+        .then(() => console.log('mongoose connect success')
+        )
+    } catch (error) {
+        
+        console.log('mongoose connect faild', error);
+        
+    }
+    
+}
+
