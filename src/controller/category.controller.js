cc4b7ba63f1d430b56ec60b843f8bc4d2efa1a56
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-20 00:12:36 +0600

    get All category

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -3,7 +3,6 @@ import CategoryModel from "../model/category.model.js";
 export const addCategory = async (req, res) => {
   try {
     const { name, image } = req.body;
-    console.log(name, image);
 
     if (!name || !image) {
       throw new Error("Please fill in all fields");
@@ -11,17 +10,26 @@ export const addCategory = async (req, res) => {
 
     const newCategory = new CategoryModel({ name, image });
     await newCategory.save();
-    res
-      .status(201)
-      .json({
-        message: "Category created successfully",
-        error: false,
-        success: true,
-        newCategory,
-      });
+    res.status(201).json({
+      message: "Category created successfully",
+      error: false,
+      success: true,
+      newCategory,
+    });
   } catch (error) {
-    console.log(error);
+    return res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
 
+export const getAllCategories = async (req, res) => {
+  try {
+    const categorys = await CategoryModel.find();
+    res.status(201).json({
+      categorys,
+    });
+  } catch (error) {
     return res
       .status(500)
       .json({ msg: error.message || error, error: true, success: false });
--- src/router/category.route.js
+++ src/router/category.route.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
-import { addCategory } from '../controller/category.controller.js'
+import { addCategory, getAllCategories } from '../controller/category.controller.js'
 
 const categoryRouter = Router()
 categoryRouter.post('/add-category',addCategory)
+categoryRouter.get('/all-category',getAllCategories)
 
 
 export default categoryRouter;  //exporting the router to use in other files  //exporting the router
