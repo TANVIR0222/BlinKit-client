633a289233877472a00c6be360e119b0bbc1a1a8
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-20 12:47:40 +0600

    add  delete category

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -37,21 +37,49 @@ export const getAllCategories = async (req, res) => {
 };
 
 export const updateCategory = async (req, res) => {
-    try {
-      const {id} = req.params
-      const { name, image } = req.body;      
+  try {
+    const { id } = req.params;
+    const { name, image } = req.body;
 
-      const updateCategory = await CategoryModel.findByIdAndUpdate(id, {name, image}, {new: true});
-      res.status(201).json({
-        message: "Category updated successfully",
-        error: false,
-        success: true,
-        updateCategory,
-        });
-
-    } catch (error) {
-      return res
+    const updateCategory = await CategoryModel.findByIdAndUpdate(
+      id,
+      { name, image },
+      { new: true }
+    );
+    res.status(201).json({
+      message: "Category updated successfully",
+      error: false,
+      success: true,
+      updateCategory,
+    });
+  } catch (error) {
+    return res
       .status(500)
       .json({ msg: error.message || error, error: true, success: false });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res
+        .status(400)
+        .json({ msg: "Category id is required", error: true });
     }
-}
+
+    const deleteProduct = await CategoryModel.findByIdAndDelete(id);
+
+    res.status(201).json({
+      message: "Category Delete successfully",
+      error: false,
+      success: true,
+      deleteProduct,
+    });
+    
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
--- src/router/category.route.js
+++ src/router/category.route.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { addCategory, getAllCategories, updateCategory } from '../controller/category.controller.js'
+import { addCategory, deleteCategory, getAllCategories, updateCategory } from '../controller/category.controller.js'
 
 const categoryRouter = Router()
 categoryRouter.post('/add-category',addCategory)
 categoryRouter.get('/all-category',getAllCategories)
 categoryRouter.put('/update-category/:id',updateCategory)
+categoryRouter.delete('/delete-category/:id',deleteCategory)
 
 
 export default categoryRouter;  //exporting the router to use in other files  //exporting the router
