511674d326488a65921c1aea53de3a1a602bc99f
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-21 12:40:41 +0600

    add delete sub category

--- src/controller/subCategory.controller.js
+++ src/controller/subCategory.controller.js
@@ -1,57 +1,78 @@
 import SubCategoryModel from "../model/subCategory.model.js";
 
-export const addSubCategory = async(req,res)=>{
-    try {
-        const { name, image ,category} = req.body 
+export const addSubCategory = async (req, res) => {
+  try {
+    const { name, image, category } = req.body;
 
-        console.log(name,image,category);
-        
-        if(!name && !image && !category[0]){
-            return res.status(400).json({
-                message : "Provide name, image, category",
-                error : true,
-                success : false
-            })
-        }
+    console.log(name, image, category);
 
-        const payload = {
-            name,
-            image,
-            category
-        }
-
-        const createSubCategory = new SubCategoryModel(payload)
-        const save = await createSubCategory.save()
-
-        return res.json({
-            message : "Sub Category Created",
-            data : save,
-            error : false,
-            success : true
-        })
-
-    } catch (error) {
-        // console.log(error);
-        return res.status(500).json({
-            message : error.message || error,
-            error : true,
-            success : false
-        })
+    if (!name && !image && !category[0]) {
+      return res.status(400).json({
+        message: "Provide name, image, category",
+        error: true,
+        success: false,
+      });
     }
-}
 
+    const payload = {
+      name,
+      image,
+      category,
+    };
+
+    const createSubCategory = new SubCategoryModel(payload);
+    const save = await createSubCategory.save();
+
+    return res.json({
+      message: "Sub Category Created",
+      data: save,
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    // console.log(error);
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
 
 export const getAllSubCategories = async (req, res) => {
-    try {
-      const categorys = await SubCategoryModel.find();
-      res.status(201).json({
-        categorys,
-      });
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ msg: error.message || error, error: true, success: false });
-    }
-  };
+  try {
+    const categorys = await SubCategoryModel.find();
+    res.status(201).json({
+      categorys,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
 
-  
+export const deleteSubCategory = async (req, res) => {
+  try {
+    const { id } = req.params;    
+    if (!id) {
+      return res.status(400).json({
+        message: "Id not found",
+        error: true,
+        success: false,
+      });
+    }
+
+    const deleteSubCategory = await SubCategoryModel.findByIdAndDelete(id);
+    res.status(201).json({
+      message: "Delete Sub Category SuccessFull",
+      deleteSubCategory,
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
--- src/router/subCategory.route.js
+++ src/router/subCategory.route.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
-import { addSubCategory, getAllSubCategories } from '../controller/subCategory.controller.js';
+import { addSubCategory, deleteSubCategory, getAllSubCategories } from '../controller/subCategory.controller.js';
 
 const subCategoryRoute = Router();
 
 subCategoryRoute.post("/add-sub-category", addSubCategory);
 subCategoryRoute.get("/all-sub-category", getAllSubCategories);
+subCategoryRoute.delete("/delete-sub-category/:id", deleteSubCategory);
 
 export default subCategoryRoute
