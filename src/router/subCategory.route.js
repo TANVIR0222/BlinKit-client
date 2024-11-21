186d390afbc3beb204810fbe69227e216c2f402e
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-21 14:21:01 +0600

    update Sub Category  route

--- src/router/subCategory.route.js
+++ src/router/subCategory.route.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { addSubCategory, deleteSubCategory, getAllSubCategories } from '../controller/subCategory.controller.js';
+import { addSubCategory, deleteSubCategory, getAllSubCategories, updateSubCategory } from '../controller/subCategory.controller.js';
 
 const subCategoryRoute = Router();
 
 subCategoryRoute.post("/add-sub-category", addSubCategory);
 subCategoryRoute.get("/all-sub-category", getAllSubCategories);
 subCategoryRoute.delete("/delete-sub-category/:id", deleteSubCategory);
+subCategoryRoute.put("/update-sub-category/:id", updateSubCategory);
 
 export default subCategoryRoute
