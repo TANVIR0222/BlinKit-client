0c26552b2cf1a573a9e8f0a6cda88cd177c7aa95
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-23 11:43:11 +0600

    all products with pagination and search

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -15,13 +15,21 @@ export const createProduct = async (req, res) => {
       more_details,
     } = req.body;
 
-    if(!name || !image[0] || !category[0] || !subCategory[0] || !unit || !price || !description ){
+    if (
+      !name ||
+      !image[0] ||
+      !category[0] ||
+      !subCategory[0] ||
+      !unit ||
+      !price ||
+      !description
+    ) {
       return response.status(400).json({
-        message : "Enter required fields",
-        error : true,
-        success : false
-    })
-}
+        message: "Enter required fields",
+        error: true,
+        success: false,
+      });
+    }
 
     const product = await ProductModel({
       name,
@@ -41,6 +49,37 @@ export const createProduct = async (req, res) => {
       error: false,
       success: true,
       product,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
+
+export const getAllProducts = async (req, res) => {
+  // API to fetch all products with pagination and search
+  try {
+    const { page = 1, limit = 10, search = "" } = req.query;
+
+    // Build search query
+    const searchQuery = search
+      ? {name: { $regex: search, $options: "i" }} // Case-insensitive search
+      : {};
+
+    // Fetch products with pagination
+    const products = await ProductModel.find(searchQuery)
+      .skip((page - 1) * limit)
+      .limit(parseInt(limit));
+
+    // Total count for pagination
+    const total = await ProductModel.countDocuments(searchQuery);
+
+    res.json({
+      products,
+      total,
+      page: parseInt(page),
+      pages: Math.ceil(total / limit),
     });
   } catch (error) {
     return res
--- src/router/product.route.js
+++ src/router/product.route.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
-import { createProduct } from '../controller/product.controller.js';
+import { createProduct, getAllProducts } from '../controller/product.controller.js';
 
 const productRouter = Router();
 
 productRouter.post('/add-product' , createProduct)
+productRouter.get('/all-product' , getAllProducts)
 
 export default productRouter;  //exportando o router para ser usado em outro arquivo.  //export
