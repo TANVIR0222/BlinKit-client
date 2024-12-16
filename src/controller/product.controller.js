c4073a2fb5eafbc43de66b3949fc0f2715314743
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-16 15:40:14 +0600

    delete Product controller

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -257,3 +257,25 @@ export const updateProductDeatils = async (req, res) => {
   }
 };
 
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({
+        message: "Id not found",
+        error: true,
+        success: false,
+      });
+    }
+    const deleteProduct = await ProductModel.findByIdAndDelete(id);
+    res.status(201).json({
+      message: "Delete Sub Category SuccessFull",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: error.message || error, error: true, success: false });
+  }
+};
